81e8b367e13bed0b581496bccd715d9ff865f171
Minimax Femto <charlemax10@gmail.com>
2025-09-18 11:12:56 +0200

    correction commit 2

--- socketUDP/Form1.cs
+++ socketUDP/Form1.cs
@@ -27,7 +27,6 @@ namespace socketUDP
 
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
@@ -46,13 +45,6 @@ namespace socketUDP
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.richTextBox3 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
             // 
             // button6
             // 
